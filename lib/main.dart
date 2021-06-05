bfffaa2c46cbfbafc4e1decf90bca99a42d6b533
Haeseong Jeon <seong0428@gmail.com>
2021-06-05 23:49:43 +0900

    실습: 빼기, 곱하기, 나누기 추가

--- lib/main.dart
+++ lib/main.dart
@@ -17,9 +17,22 @@ class WidgetApp extends StatefulWidget {
 }
 
 class _WidgetExampleState extends State<WidgetApp> {
+  List<String> _buttonList = ['더하기', '빼기', '곱하기', '나누기'];
+  List<DropdownMenuItem<String>> _dropdownMenuItems =
+      List.empty(growable: true);
+  String? _buttonText = '';
   String sum = '';
   TextEditingController value1 = TextEditingController();
   TextEditingController value2 = TextEditingController();
+
+  @override
+  void initState() {
+    super.initState();
+    for (var item in _buttonList) {
+      _dropdownMenuItems.add(DropdownMenuItem(value: item, child: Text(item)));
+    }
+    _buttonText = _dropdownMenuItems[0].value;
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -53,15 +66,40 @@ class _WidgetExampleState extends State<WidgetApp> {
                 padding: EdgeInsets.all(15),
                 child: RaisedButton(
                     child: Row(
-                      children: [Icon(Icons.add), Text('더하기')],
+                      children: [
+                        Icon(Icons.add),
+                        Text(_buttonText != null ? _buttonText! : ''),
+                      ],
                     ),
                     color: Colors.amber,
                     onPressed: () {
                       setState(() {
-                        int result = int.parse(value1.value.text) + int.parse(value2.value.text);
+                        var value1Int = int.parse(value1.value.text);
+                        var value2Int = int.parse(value2.value.text);
+                        var result;
+                        if (_buttonText == '더하기') {
+                          result = value1Int + value2Int;
+                        } else if (_buttonText == '빼기') {
+                          result = value1Int - value2Int;
+                        } else if (_buttonText == '곱하기') {
+                          result = value1Int * value2Int;
+                        } else {
+                          result = value1Int / value2Int;
+                        }
                         sum = result.toString();
                       });
-                    }))
+                    })),
+            Padding(
+                padding: EdgeInsets.all(15),
+                child: DropdownButton(
+                  items: _dropdownMenuItems,
+                  onChanged: (value) {
+                    setState(() {
+                      _buttonText = value.toString();
+                    });
+                  },
+                  value: _buttonText,
+                ))
           ],
         )),
       ),
